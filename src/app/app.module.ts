b140c69fdeba78aa7eeb3c9232be39312ece8068
Tutku Ince <tutku.ince@outlook.com>
2024-01-04 22:52:44 +0300

    application logo and navigation styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./shared/header/header.component";
-import { TestcomponentComponent } from './shared/testcomponent/testcomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    TestcomponentComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
