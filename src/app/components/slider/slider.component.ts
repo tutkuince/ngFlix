5deb88a2de3a7f4afe8186e35447d14f8ff2bc68
Tutku Ince <tutku.ince@outlook.com>
2024-01-06 22:12:08 +0300

    Getting Populer movies

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
 
+  constructor(private http: HttpClient) {}
 }
