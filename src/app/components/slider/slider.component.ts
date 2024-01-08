22794e0000dcfaedd0813f653b3ee9320672feac
Tutku Ince <tutku.ince@outlook.com>
2024-01-08 22:48:14 +0300

    Change Slide Every 5 seconds

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MoviesService} from "../../services/movies.service";
 
 @Component({
@@ -6,9 +6,17 @@ import {MoviesService} from "../../services/movies.service";
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit{
   constructor(private moviesService: MoviesService) {
   }
   // $ suffix means this is Observable<Object>
   movies$ = this.moviesService.getPopularMovies();
+
+  slideIndex: number = 0;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.slideIndex++;
+    }, 5000);
+  }
 }
